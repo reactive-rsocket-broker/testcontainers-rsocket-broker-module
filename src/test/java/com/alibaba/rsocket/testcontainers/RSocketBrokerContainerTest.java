9ed1a410e63e5aef3513fe974dbc68a639d2defa
linux_china <libing.chen@gmail.com>
2021-03-27 17:10:49 -0700

    add health checker

--- src/test/java/com/alibaba/rsocket/testcontainers/RSocketBrokerContainerTest.java
+++ src/test/java/com/alibaba/rsocket/testcontainers/RSocketBrokerContainerTest.java
@@ -5,14 +5,21 @@ import org.junit.jupiter.api.Test;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 @Testcontainers
 public class RSocketBrokerContainerTest {
     @Container
     static RSocketBrokerContainer rsocketBrokerContainer = new RSocketBrokerContainer();
 
     @Test
-    public void shouldAnswerWithTrue() {
-        System.out.println(rsocketBrokerContainer.getRSocketBrokerUri());
-        System.out.println(rsocketBrokerContainer.getWebConsoleUri());
+    public void testBrokerIsReady() throws Exception {
+        System.out.println("RSocket Server URI: " + rsocketBrokerContainer.getRSocketBrokerUri());
+        System.out.println("RSocket WebConsole URI: " + rsocketBrokerContainer.getWebConsoleUri());
+        URL healthURL = new URL(rsocketBrokerContainer.getActuatorUri() + "/actuator/health");
+        HttpURLConnection urlConnection = (HttpURLConnection) healthURL.openConnection();
+        int code = urlConnection.getResponseCode();
+        System.out.println("Health check response code: " + code);
     }
 }
