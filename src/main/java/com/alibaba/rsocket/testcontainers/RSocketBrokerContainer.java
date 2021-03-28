7eeb50d66d71e00e9b22924da8afe79f1f22ed51
linux_china <libing.chen@gmail.com>
2021-03-27 17:21:11 -0700

    Add javadoc

--- src/main/java/com/alibaba/rsocket/testcontainers/RSocketBrokerContainer.java
+++ src/main/java/com/alibaba/rsocket/testcontainers/RSocketBrokerContainer.java
@@ -13,14 +13,27 @@ public class RSocketBrokerContainer extends GenericContainer<RSocketBrokerContai
     private static final DockerImageName DEFAULT_IMAGE_NAME = DockerImageName.parse("linuxchina/alibaba-rsocket-broker");
     private static final String DEFAULT_TAG = "1.0.1";
 
+    /**
+     * construct RSocket broker container from "linuxchina/alibaba-rsocket-broker:1.0.1"
+     */
     public RSocketBrokerContainer() {
         this(DEFAULT_IMAGE_NAME.withTag(DEFAULT_TAG));
     }
 
+    /**
+     * construct RSocket broker container from image name
+     *
+     * @param dockerImageName docker image name, such as "linuxchina/alibaba-rsocket-broker:1.0.0"
+     */
     public RSocketBrokerContainer(final String dockerImageName) {
         this(DockerImageName.parse(dockerImageName));
     }
 
+    /**
+     * construct RSocket broker container from DockerImageName object
+     *
+     * @param dockerImageName DockerImageName object
+     */
     public RSocketBrokerContainer(final DockerImageName dockerImageName) {
         super(dockerImageName);
         dockerImageName.assertCompatibleWith(DEFAULT_IMAGE_NAME);
@@ -28,17 +41,32 @@ public class RSocketBrokerContainer extends GenericContainer<RSocketBrokerContai
         waitingFor(Wait.forListeningPort());
     }
 
+    /**
+     * get RSocket broker connection uri
+     *
+     * @return connection uri, format as "tcp://localhost:xxxx"
+     */
     public String getRSocketBrokerUri() {
         Integer rsocketMappedPort = getMappedPort(9999);
         return "tcp://localhost:" + rsocketMappedPort;
     }
 
-    public String getWebConsoleUri() {
+    /**
+     * get RSocket broker web console URL
+     *
+     * @return connection uri, format as "http://localhost:xxxx"
+     */
+    public String getWebConsoleURL() {
         Integer webExposedPort = getMappedPort(9998);
         return "http://localhost:" + webExposedPort;
     }
 
-    public String getActuatorUri() {
+    /**
+     * get RSocket broker Spring Boot actuator URL
+     *
+     * @return connection uri, format as "http://localhost:xxxx"
+     */
+    public String getActuatorURL() {
         Integer managementExposedPort = getMappedPort(9997);
         return "http://localhost:" + managementExposedPort;
     }
